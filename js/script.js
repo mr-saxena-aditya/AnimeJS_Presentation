edc3a7593d093ea8559ffdbd99472540e4e7b148
Aditya  Saxena <mr.saxena.aditya@gmail.com>
2023-07-19 16:55:51 -0400

    added full screen animation

--- js/script.js
+++ js/script.js
@@ -100,3 +100,47 @@ function typeLogo() {
       typeLogo();
     }, 2000); // 2000 milliseconds = 2.0 seconds
   });
+
+
+// Function to make the screen full screen
+const fullscreenIcon = document.getElementById('fullscreen-icon');
+let isFullScreen = false;
+
+fullscreenIcon.addEventListener('click', () => {
+  if (!isFullScreen) {
+    enterFullscreen();
+  } else {
+    exitFullscreen();
+  }
+});
+
+function enterFullscreen() {
+  const element = document.documentElement || document.body;
+  if (element.requestFullscreen) {
+    element.requestFullscreen();
+  } else if (element.webkitRequestFullscreen) {
+    element.webkitRequestFullscreen();
+  } else if (element.mozRequestFullScreen) {
+    element.mozRequestFullScreen();
+  } else if (element.msRequestFullscreen) {
+    element.msRequestFullscreen();
+  }
+
+  isFullScreen = true;
+  fullscreenIcon.innerHTML = '<i class="icon fa fa-compress"></i>';
+}
+
+function exitFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen();
+  } else if (document.mozCancelFullScreen) {
+    document.mozCancelFullScreen();
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen();
+  }
+
+  isFullScreen = false;
+  fullscreenIcon.innerHTML = '<i class="icon fa fa-arrows-alt"></i>';
+}
